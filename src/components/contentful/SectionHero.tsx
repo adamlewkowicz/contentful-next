569d9168b67413829875120b2664dcd0bb4fb9ff
Adam <adam.lewkowicz@bejamas.io>
2023-04-17 14:11:49 +0200

    feat: adds section hero component

---
+++ src/components/contentful/SectionHero.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import type { Asset } from '@/network/getPageData';
+
+interface SectionHeroProps {
+  title: string;
+  subtitle: string;
+  background: Asset;
+}
+
+export function SectionHero({ title, subtitle, background }: SectionHeroProps) {
+  return (
+    <Container>
+      <Content>
+        <h2>{title}</h2>
+        <p>{subtitle}</p>
+      </Content>
+      <Img
+        loading="lazy"
+        src={background.url}
+        alt={background.title}
+        width={background.width}
+        height={background.height}
+      />
+    </Container>
+  );
+}
+
+const Container = styled.section`
+  position: relative;
+  height: 100vh;
+`;
+
+const Content = styled.div`
+  color: #fff;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateX(-50%);
+  text-align: center;
+  font-size: 2rem;
+`;
+
+const Img = styled.img`
+  width: 100%;
+  height: 100%;
+`;
