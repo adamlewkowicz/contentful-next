5961bb8a89cd230e334c2f970fe1175df4d9e919
Adam <adam.lewkowicz@bejamas.io>
2023-04-18 12:30:14 +0200

    feat: adds font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import Head from 'next/head';
 import { Inter } from 'next/font/google';
+import { DM_Sans as Font } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
 import { ComponentConnector } from '@/utils/componentSelector';
 import { type GetServerSideProps } from 'next';
 import { getPageData } from '@/network/getPageData';
-import type { PageData } from '@/network/getPageData';
+import { type PageData } from '@/network/getPageData';
 import { Layout } from '@/components/main';
 
-const inter = Inter({ subsets: ['latin'] });
+const font = Font({ subsets: ['latin'], weight: ['400', '500', '700'] });
 
 interface HomeProps {
   data: PageData;
@@ -18,11 +19,13 @@ export default function Home({ data }: HomeProps) {
   const { metatags } = page;
 
   return (
-    <Layout metatags={metatags}>
-      {page.body.items.map((comp: any) => (
-        <ComponentConnector key={comp.__typename} {...comp} />
-      ))}
-    </Layout>
+    <div style={font.style}>
+      <Layout metatags={metatags}>
+        {page.body.items.map((comp: any) => (
+          <ComponentConnector key={comp.name ?? comp.__typename} {...comp} />
+        ))}
+      </Layout>
+    </div>
   );
 }
 
